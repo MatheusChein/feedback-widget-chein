898cf8b6e201f6cbdd16e7c5fbe44609ca8c2ba6
Matheus Chein <matheus.chein@fabric.inc>
2022-05-04 22:09:08 -0300

    styling feedback steps

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      src: 'images/bug.svg',
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      src: 'images/idea.svg',
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      src: 'images/thought.svg',
+      alt: 'Imagem de um balão de pensamento',
+    },
+  },
+};
+
+// Object.entries(feedbackTypes) => [
+//    ['BUG', { title: 'Problema', image: { src: bugImg, alt: 'Imagem de um inseto' } }],
+//    ['IDEA', {...}],
+//    ['OTHER', {...}]
+// ]
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onRestartFeedback={handleRestartFeedback}
+        />
+      )}
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{' '}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
